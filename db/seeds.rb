c5caeaa644a086e89038ca2a57302d6d4d6021dc
Abhishek Singh <abhishek111204cse@gmail.com>
2018-08-06 18:42:35 +0530

    Integrated action for index itens

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |x|
+	Blog.create!(
+       title:"My blog post #{x}",
+       body:"djfbdjfbjdfbjdbfjdbfjdbfjdbfjdfjbf"
+	)
+end
+
+puts "10 blogs created"
+
+5.times do |y|
+	Skill.create!(
+  title:"My Skills #{y}",
+	percent_utilized:15
+	)
+end
+
+puts "5 skills created"
+
+9.times do |z|
+
+Portfolio.create!(
+	title:"My Skills #{z}",
+	subtitle:"My Sub #{z}",
+	body:"dfdfdfdfdfdfdfdfd",
+	main_image:"http://via.placeholder.com/350x150",
+	thumb_image:"http://via.placeholder.com/150x150"
+		)
+	end
+
+puts "9 portolios created created"
+
+
