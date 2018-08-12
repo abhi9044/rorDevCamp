c806f97ab4775fc1d2123a46e4b1cbc4489727f3
Abhishek Singh <abhishek111204cse@gmail.com>
2018-08-12 17:51:36 +0530

    Updated status enum

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -43,6 +43,13 @@ class PortfoliosController < ApplicationController
       end
     end
   end
-
+ def destroy
+ 	@portfolio_item = Portfolio.find(params[:id])
+    @portfolio_item.destroy
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Record was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   resources :portfolios
-  get 'pages/home'
-  get 'pages/about'
+  get "about", to: "pages#about"
   get 'pages/contact'
   resources :blogs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to:"pages#home"
 end
